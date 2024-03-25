f46cc51f76bcfe37942b1c1507305d1b7c9f0fc2
Zachary <zachbloodworth@gmail.com>
2024-03-24 21:10:31 -0400

    Add SQL comments to PostService.java

--- 4370_p2_micro_blog-main/src/main/java/uga/menik/cs4370/services/PostService.java
+++ 4370_p2_micro_blog-main/src/main/java/uga/menik/cs4370/services/PostService.java
@@ -49,6 +49,8 @@ public class PostService {
      * with id userIdToExclude.
      */
     public void makePost(String userId, String postText) throws SQLException {
+        // this SQL statement is used to push new posts into the database, and it is called from the
+        // home page: http://localhost:8081/
         final String sql = "insert into post (userId, postDate, postText) values (?, ?, ?)";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -74,6 +76,9 @@ public class PostService {
      * user, which is used to check for any posts from users that they follow.
      */
     public List<Post> getPostsFromFollowed(int userId) throws SQLException{
+        // this SQL statement is used to retrieve a list of posts from all users that the currently logged in user follows
+        // the statement is called when the home page http://localhost:8081/ is loaded, in order to retrieve the posts
+        // to be displayed
         final String sql = "select * from post, follow where follow.followerUserId = ? and post.userId = follow.followeeUserId ORDER BY post.postDate DESC"; // ryan change here "order by"
         List<Post> posts = new ArrayList<Post>();
         try (Connection conn = dataSource.getConnection();
@@ -108,6 +113,10 @@ public class PostService {
      * hashtag filter provided. 
      */
     public List<Post> getAllPosts() throws SQLException{
+        // this SQL statement retrieves every post from the database, and is used in the hashtag search functionality
+        // the posts retrieved from this statement are filtered by hashtag outside of this function
+        // because the hashtag search bar is visible on every page, this SQL statement could potentially be called from
+        // any URL
         final String sql = "select * from post";
         List<Post> posts = new ArrayList<Post>();
         try (Connection conn = dataSource.getConnection();
@@ -138,6 +147,9 @@ public class PostService {
      * @return
      */
     private int getHeartsCount(String postId) throws SQLException {
+        // this SQL statement is used to get a count of the exact number of likes which exist in the database
+        // for the specific post denoted by postId, and it is called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "select count(*) from heart where postId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -153,6 +165,9 @@ public class PostService {
     }
 
     private int getCommentsCount(String postId) throws SQLException {
+        // this SQL statement is used to get a count of the exact number of comments which exist in the database
+        // for the specific post denoted by postId, and it is called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "select count(*) from comment where postId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -168,6 +183,9 @@ public class PostService {
     }
 
     private boolean getIsHearted(String postId, String userId) throws SQLException {
+        // this SQL statement is used to determine whether or not the post denoted by postId is liked by the current user,
+        // and it is called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "select * from heart where postId = ? and userId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -192,6 +210,9 @@ public class PostService {
     }
 
     private boolean getIsBookmarked(String postId, String userId) throws SQLException {
+        // this SQL statement is used to determine whether or not the post denoted by postId is bookmarked by the current user,
+        // and it is called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "select * from bookmark where postId = ? and userId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -208,6 +229,9 @@ public class PostService {
     }
 
     public void addLike(String postId, String userId) throws SQLException {
+        // this SQL statement is used to add a like from the current user for the post denoted by postId,
+        // and it can be called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "insert into heart values (?,?)";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -224,6 +248,9 @@ public class PostService {
     }
 
     public void removeLike(String postId, String userId) throws SQLException {
+        // this SQL statement is used to remove a like from the current user for the post denoted by postId,
+        // and it can be called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "delete from heart where postId = ? and userId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -240,6 +267,9 @@ public class PostService {
     }
 
     public void addBookmark(String postId, String userId) throws SQLException {
+        // this SQL statement is used to add a bookmark from the current user for the post denoted by postId,
+        // and it can be called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "insert into bookmark values (?,?)";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -256,6 +286,9 @@ public class PostService {
     }
 
     public void removeBookmark(String postId, String userId) throws SQLException {
+        // this SQL statement is used to remove a bookmark from the current user for the post denoted by postId,
+        // and it can be called on any page where a post(s) is displayed
+        // http://localhost:8081, http://localhost:8081/post/{postId}, http://localhost:8081/profile, http://localhost:8081/bookmarks
         final String sql = "delete from bookmark where postId = ? and userId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -272,6 +305,9 @@ public class PostService {
     }
 
     public void makeComment(String postId, String userId, String commentText) throws SQLException {
+        // this SQL statement is used to add a comment from the current user on the post denoted by postId,
+        // and it is called from the post page (http://localhost:8081/post/{postId}), because this is the page
+        // where users are able to enter comments
         final String sql = "insert into comment (postId, userId, commentDate, commentText) values (?, ?, ?, ?)";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -306,6 +342,9 @@ public class PostService {
     private List<Comment> getCommentsForPost(String postId) throws SQLException {
         List<Comment> postComments = new ArrayList<Comment>();
 
+        // this SQL statement is used to retrieve all comments for the post denoted by postId,
+        // and it is called from the post page (http://localhost:8081/post/{postId}), because this is the page
+        // where comments are displayed in-line with the post
         final String sql = "select * from comment where postId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -326,6 +365,9 @@ public class PostService {
     }
 
     private ExpandedPost getExpandedPost(List<Comment> comments, String postId) throws SQLException {
+        // this SQL statement is used to retrieve the specific post denoted by postId, and it is called on the
+        // post page (http://localhost:8081/post/{postId}) because this is the only page where a singular specific
+        // post is retrieved by postId
         final String sql = "select * from post where postId = ?";
         try (Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -351,8 +393,11 @@ public class PostService {
             throw new SQLException();
         }
     }
+
     // test ryan
     public List<Post> getBookmarkedPosts(String userId) throws SQLException {
+        // this SQL statement is used to retrieve all posts which the current user has bookmarked, and it is called on
+        // the bookmarks page (http://localhost:8081/bookmarks)
         final String sql = "SELECT * FROM post INNER JOIN bookmark ON post.postId = bookmark.postId WHERE bookmark.userId = ?";
         List<Post> bookmarkedPosts = new ArrayList<>();
         try (Connection conn = dataSource.getConnection();
