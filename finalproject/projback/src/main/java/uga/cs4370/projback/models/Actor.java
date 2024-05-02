a0cfab7cf8c1cd1ef9728367e2babe63cc7560c8
JudeMullins <131062829+JudeMullins@users.noreply.github.com>
2024-05-02 14:34:00 -0400

    Update Actor.java

--- finalproject/projback/src/main/java/uga/cs4370/projback/models/Actor.java
+++ finalproject/projback/src/main/java/uga/cs4370/projback/models/Actor.java
@@ -1,5 +1,34 @@
 package uga.cs4370.projback.models;
 
 public class Actor {
-    
+    private final String actorId;
+
+    private final String fname;
+
+    private final String lname;
+
+    private final String age;
+
+    public Actor(String actorId, String fname, String lname, String age) {
+        this.actorId = actorId;
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+    }
+
+    public String getActorId() {
+        return actorId;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getAge() {
+        return age;
+    }
 }
