2529fc50d4844afeaf765526c32c5c204d144e89
JudeMullins <131062829+JudeMullins@users.noreply.github.com>
2024-05-02 14:36:36 -0400

    Update Movie.java

--- finalproject/projback/src/main/java/uga/cs4370/projback/models/Movie.java
+++ finalproject/projback/src/main/java/uga/cs4370/projback/models/Movie.java
@@ -1,5 +1,34 @@
 package uga.cs4370.projback.models;
 
 public class Movie {
-    
+    private final String movieId;
+
+    private final String title;
+
+    private final String genre;
+
+    private final String releaseDate;
+
+    public Movie(String movieId, String title, String genre, String releaseDate) {
+        this.movieId = movieId;
+        this.title = title;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
 }
