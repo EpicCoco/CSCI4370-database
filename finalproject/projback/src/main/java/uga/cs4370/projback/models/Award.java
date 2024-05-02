5114b1a35bdfb5cbda51ec3875ddfe2aa52579bb
JudeMullins <131062829+JudeMullins@users.noreply.github.com>
2024-05-02 14:34:24 -0400

    Update Award.java

--- finalproject/projback/src/main/java/uga/cs4370/projback/models/Award.java
+++ finalproject/projback/src/main/java/uga/cs4370/projback/models/Award.java
@@ -1,5 +1,34 @@
 package uga.cs4370.projback.models;
 
 public class Award {
-    
+    private final String awardId;
+
+    private final String awardName;
+
+    private final String actorId;
+
+    private final String movieId;
+
+    public Award(String awardId, String awardName, String actorId, String movieId) {
+        this.awardId = awardId;
+        this.awardName = awardName;
+        this.actorId = actorId;
+        this.movieId = movieId;
+    }
+
+    public String getAwardId() {
+        return awardId;
+    }
+
+    public String getAwardName() {
+        return awardName;
+    }
+
+    public String getActorId() {
+        return actorId;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
 }
