5f1d6feaef9e8767812e2aa5bb3596379ec874f8
Ryan Albright <rpa56776@uga.edu>
2024-05-01 22:27:20 -0400

    changes to database setup

--- 4370_p2_micro_blog-main/database_setup.sql
+++ 4370_p2_micro_blog-main/database_setup.sql
@@ -4,85 +4,62 @@ create database if not exists cs4370_mb_platform;
 -- Use the created database.
 use cs4370_mb_platform;
 
--- Create the user table.
-create table if not exists user (
-    userId int auto_increment,
-    username varchar(255) not null unique,
-    password varchar(255) not null,
-    firstName varchar(255) not null,
-    lastName varchar(255) not null,
-    primary key (userId),
-    unique (username),
-    constraint userName_min_length check (char_length(trim(userName)) >= 2),
-    constraint firstName_min_length check (char_length(trim(firstName)) >= 2),
-    constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
-);
 
--- Create post table 
-create table if not exists post (
-    postId int not null auto_increment,
-    userId int not null,
-    postDate datetime,
-    postText varchar(225) not null,
+-- Create Award Table
+create table if not exists MovieAward (
+    AwardID int auto_increment not null unique,
+    AwardName varchar(20) not null unique
+)
 
-    primary key (postId),
-    foreign key (userId) references user(userId)
-);
+create table if not exists ActorAward (
+    ActorAwardID int auto_increment unique,
+    ActorAwardName varchar(20) unique
+)
 
--- Create comment table
-create table if not exists comment (
-    commentId int not null auto_increment,
-    postId int not null,
-    userId int not null,
-    commentDate datetime,
-    commentText varchar(500),
-
-    primary key(commentId),
-    foreign key(userId) references user(userId),
-    foreign key(postId) references post(postId)
-);
-
--- Create heart table
-create table if not exists heart (
-    postId int not null,
-    userId int not null,
-
-    primary key (postId, userId),
-    foreign key(userId) references user(userId),
-    foreign key(postId) references post(postId)
-);
-
--- Create bookmark table
-create table if not exists bookmark (
-    postId int not null,
-    userId int not null,
-
-    primary key (postId, userId),
-    foreign key(postId) references post(postId),
-    foreign key(userId) references user(userId)
-);
-
--- Create hashtag table ***
-create table if not exists hashtag (
-    hashTag varchar(225),
-    postId int not null,
-
-    primary key(hashTag, postId),
-    constraint hashTag_check check (hashTag like'%#%'),
-    foreign key(postId) references post(postId)
-);
-
--- Create follow
-create table if not exists follow (
-    followerUserId int,
-    followeeUserId int,
-
-    primary key(followerUserId,followeeUserId),
-    foreign key(followeeUserId) references user(userId),
-    foreign key(followerUserId) references user(userId)
-);
+-- Create Actor Table
+create table if not exists Actor (
+    ActorID int auto_increment not null unique,
+    fName varchar(20) not null,
+    lname varchar(20) not null,
+    age int not null,
+    ActorAwardID int auto_increment not null unique,
 
 
+    foreign key(ActorAwardID) references ActorAward(ActorAwardID)
+)
+
+-- Create Movie Table
+create table if not exists Movie (
+    MovieID int auto_increment not null,
+    Title varchar(50) not null,
+    Genre varchar(30),
+    ReleaseDate date,
+    AwardID int,
+    ReviewID int not null,
+
+    foreign key (AwardID) references Award(AwardID),
+    foreign key (ReviewID) references Review(ReviewID)
+)
+
+-- Create Review table
+create table if not exists Review (
+    ReviewID int auto_increment not null,
+    rating varchar(10),
+    comment varchar(200)
+)
+
+-- Create User table
+create table if not exists User (
+    UserID int auto_increment not null,
+    fName varchar(20) not null,
+    lName varchar(20) not null,
+    ReviewID int not null,
+
+    foreign key (ReviewID) references Review(ReviewID)
+)
+
+
+-- TODO
 -- Insert Data into the Database
 
 insert into user (username, password, firstName, lastName) values ('johnnyBoy', 'johnnyBoy1', 'Jon', 'Bon Jovi');
