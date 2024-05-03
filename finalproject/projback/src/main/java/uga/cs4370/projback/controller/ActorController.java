cbde1b2b728079caa18b655a3081cbe7bc1f24ec
Ryan Albright <rpa56776@uga.edu>
2024-05-03 01:06:12 -0400

    ActorController

--- finalproject/projback/src/main/java/uga/cs4370/projback/controller/ActorController.java
+++ finalproject/projback/src/main/java/uga/cs4370/projback/controller/ActorController.java
@@ -1,51 +1,71 @@
 package uga.cs4370.projback.controller;
 
-import uga.cs4370.projback.models.*;
+import uga.cs4370.projback.models.Actor;
+import uga.cs4370.projback.models.Movie;
+import uga.cs4370.projback.models.Award;
+import uga.cs4370.projback.services.ActorService;
+import uga.cs4370.projback.services.MovieService; // TODO
+import uga.cs4370.projback.services.AwardService; // TODO
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ActorController {
-    private List<Actor> actors;
 
-    public ActorController() {
-        this.actors = new ArrayList<>();
+    private final ActorService actorService;
+    private final MovieService movieService;
+    private final AwardService awardService;
+
+    @Autowired
+    public ActorController(ActorService actorService, MovieService movieService, AwardService awardService) {
+        this.actorService = actorService;
+        this.movieService = movieService;
+        this.awardService = awardService;
     }
 
-    // Add a new actor to the list
-    public void addActor(Actor actor) {
-        actors.add(actor);
+    /**
+     * 
+     * Get all movies with a certain actor in it
+     */
+    @GetMapping("/actors/{actorId}/movies")
+    public ModelAndView getMoviesWithActor(@PathVariable("actorId") int actorId) {
+        ModelAndView mv = new ModelAndView("actor_movies");
+        Actor actor = actorService.getActorById(actorId);
+        List<Movie> moviesWithActor = movieService.getMoviesByActor(actor);
+        mv.addObject("actor", actor);
+        mv.addObject("movies", moviesWithActor);
+        return mv;
     }
 
-    // Get all movies with a certain actor in it
-    public List<Movie> getMoviesWithActor(String actorName) {
-        List<Movie> moviesWithActor = new ArrayList<>();
-        for (Actor actor : actors) {
-            if (actor.getName().equalsIgnoreCase(actorName)) {
-                moviesWithActor.addAll(actor.getMovies());
-            }
-        }
-        return moviesWithActor;
+    /**
+     * 
+     * Get all awards for an actor
+     */
+    @GetMapping("/actors/{actorId}/awards")
+    public ModelAndView getAwardsForActor(@PathVariable("actorId") int actorId) {
+        ModelAndView mv = new ModelAndView("actor_awards");
+        Actor actor = actorService.getActorById(actorId);
+        List<Award> awardsForActor = awardService.getAwardsForActor(actor);
+        mv.addObject("actor", actor);
+        mv.addObject("awards", awardsForActor);
+        return mv;
     }
 
-    // Get all awards for an actor
-    public List<Award> getAwardsForActor(String actorName) {
-        List<Award> awardsForActor = new ArrayList<>();
-        for (Actor actor : actors) {
-            if (actor.getName().equalsIgnoreCase(actorName)) {
-                awardsForActor.addAll(actor.getAwards());
-            }
-        }
-        return awardsForActor;
-    }
-
-    // Get actor's info (name, age, etc)
-    public Actor getActorInfo(String actorName) {
-        for (Actor actor : actors) {
-            if (actor.getName().equalsIgnoreCase(actorName)) {
-                return actor;
-            }
-        }
-        return null; // Actor not found
+    /**
+     * 
+     * Get actor’s info (name, age, etc)
+     */
+    @GetMapping("/actors/{actorId}")
+    public ModelAndView getActorInfo(@PathVariable("actorId") int actorId) {
+        ModelAndView mv = new ModelAndView("actor_info");
+        Actor actor = actorService.getActorById(actorId); // TODO
+        mv.addObject("actor", actor);
+        return mv;
     }
 }
