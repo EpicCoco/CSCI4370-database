90a701bcd3c646483eb238a2a9bef30e551725e4
JudeMullins <131062829+JudeMullins@users.noreply.github.com>
2024-05-04 12:27:11 -0400

    Update UserController.java

--- finalproject/projback/src/main/java/uga/cs4370/projback/controller/UserController.java
+++ finalproject/projback/src/main/java/uga/cs4370/projback/controller/UserController.java
@@ -1,5 +1,51 @@
 package uga.cs4370.projback.controller;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.List;
+import uga.cs4370.projback.models.*;
+
+import uga.cs4370.projback.ProjbackApplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import uga.cs4370.projback.services.UserService;
+
 public class UserController {
-    
+    @Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> profileOfLoggedInUser() throws SQLException {
+        System.out.println("User is attempting to view profile of the logged in user.");
+        return profileOfSpecificUser(userService.getLoggedInUser().getUserId());
+    }
+
+     @GetMapping("/{userId}")
+    public ResponseEntity<String> profileOfSpecificUser(@PathVariable("userId") String userId) throws SQLException {
+        System.out.println("User is attempting to view profile: " + userId);
+        // See notes on ModelAndView in BookmarksController.java.
+        if (!userService.reviewList(userId).equals(null)) {
+            return ResponseEntity.ok("posts_page");
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
